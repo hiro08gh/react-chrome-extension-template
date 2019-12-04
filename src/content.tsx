a5f4649b03f0de0c49013cc02a2f082cfb6eb478
hiro08gh <hiro08gh@gmail.com>
2019-12-04 16:36:28 +0900

    first commit

---
+++ src/content.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import './contnet.css';
+
+const Main = () => {
+  return <div>App</div>;
+};
+
+const app = document.createElement('div');
+app.id = 'my-extension-root';
+document.body.appendChild(app);
+ReactDOM.render(<Main />, app);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+    content: './src/content.tsx',
+    background: './src/background.ts',
+  },
+  output: {
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {minimize: true},
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new CopyPlugin([{from: './public/manifest.json', to: 'manifest.json'}]),
+  ],
+};
